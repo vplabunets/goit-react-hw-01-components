2071f06499bf2fef107315ae91c3d47234b6fe4a
Volodymyr Labunets <vplabunets@gmail.com>
2022-10-10 22:38:41 +0300

    after reworking

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { Profile } from './Profile/ProfileCard/Profile';
-import user from './json/user.json';
+import user from '../json/user.json';
 import { Statistics } from './Statistics/Statistics';
-import data from './json/data.json';
+import data from '../json/data.json';
 import { FriendsList } from './Friends/FriendsList/Friends';
-import friends from './json/friends.json';
+import friends from '../json/friends.json';
 import { TransactionHistory } from './Transactions/TransactionHistory';
-import transactions from './json/transactions.json';
+import transactions from '../json/transactions.json';
 
 export const App = () => {
   return (
